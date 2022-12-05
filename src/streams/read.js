1b59db78a3a0e93a79df7f6b34b2e7b648c0637b
ilonaand <ilonaand@gmail.com>
2022-12-05 15:38:05 +0300

    fix: read async

--- src/streams/read.js
+++ src/streams/read.js
@@ -10,9 +10,7 @@ import { checkFileExists } from '../utils.js';
 
 const read = async () => {
     const __filename = fileURLToPath(import.meta.url);
-
     const __dirname = path.dirname(__filename);
-
     const fileName = path.join(__dirname,'files/FileToRead.txt');
 
     const check = await checkFileExists(fileName);
@@ -22,23 +20,22 @@ const read = async () => {
     }
 
     const streamRead = createReadStream(fileName, {encoding: 'utf8'});
-    
-    const data = [];
-    streamRead.on('readable', () => {
-        let chunk;
-        while (null !== (chunk = streamRead.read())){
+
+    const readableToString = async (readable) => {
+        let data = [];
+        for await (const chunk of readable) {
             data.push(chunk);
-          };
-    });
-
-    streamRead.on('end', () => {
-        process.stdout.write(data.join('').toString());
-    });
-
-    streamRead.on("error", err => {
+        }
+        return data.join('');
+      }
+  
+    try {
+      const result = await readableToString(streamRead);
+      process.stdout.write(result.toString());
+    } catch (err) {
         console.error(err);
-        process.exit(1);
-    });
+    }  
+
 };
 
 await read();
